f6a470880eb7bac66fc7b95981e8da390dacc222
MdHasib01 <md.hasibuzzaman001@gmail.com>
2025-04-12 06:38:41 +0600

    added timeout to the query

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -29,7 +29,8 @@ func (s *PostStore) Create(ctx context.Context, post *Post) error {
 	INSERT INTO posts (content, title, user_id, tags)
 	VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at
 	`
-
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
 	err := s.db.QueryRowContext(
 		ctx,
 		query,
@@ -54,6 +55,9 @@ func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 		SELECT id, user_id, title, content, created_at, updated_at, tags, version
 		FROM posts 
 		WHERE id = $1`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
 
 	var post Post
 	err := s.db.QueryRowContext(
@@ -86,6 +90,8 @@ func (s *PostStore) DeletePost(ctx context.Context, postID int64) error {
 	DELETE FROM posts 
 	WHERE id = $1
 		`
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
 
 	res, err := s.db.ExecContext(
 		ctx,
@@ -114,6 +120,8 @@ func (s *PostStore) Update(ctx context.Context, post *Post) error {
 	WHERE id = $3 AND version = $4
 	RETURNING version
 `
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
 	err := s.db.QueryRowContext(
 		ctx,
 		query,
